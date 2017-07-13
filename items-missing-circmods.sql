6c23c01a7a74a8cfbf2d25d7f66ea35668ccb195
dzoladz <derek@derekzoladz.com>
2017-07-13 11:33:48 -0400

    list barcodes for items that are missing a circulation modifier, include library

---
+++ items-missing-circmods.sql
@@ -0,0 +1,16 @@
+-- list items (barcodes) missing circulcation modifiers by library (org_unit)
+SELECT ou.name AS "Library", 
+	b.id as "TCN", 
+	cn.label as "Call Number", 
+	c.circ_modifier AS "Circulation Modifier", 
+	c.barcode AS "Barcode", 
+	xpath('//m:datafield[@tag="245"]/m:subfield[@code="a"]/text()', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]) as "Title", 'Missing circ mod' as "Issue"
+  FROM biblio.record_entry b
+	INNER JOIN asset.call_number cn ON cn.record=b.id
+	INNER JOIN asset.copy c ON c.call_number=cn.id
+	INNER JOIN actor.org_unit ou on ou.id=c.circ_lib
+  WHERE b.deleted=FALSE
+	AND c.deleted=FALSE
+	AND b.id != -1
+	AND c.circ_modifier IS NULL
+  ORDER BY ou.name, c.barcode;
