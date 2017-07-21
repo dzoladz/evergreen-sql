882ac6bb9de0febca5237d8a127b97c09ce9e494
dzoladz <derek@derekzoladz.com>
2017-07-21 12:16:09 -0400

    updating the 'issue' label

--- items-missing-circmods.sql
+++ items-missing-circmods.sql
@@ -2,9 +2,9 @@
 SELECT ou.name AS "Library", 
 	b.id as "TCN", 
 	cn.label as "Call Number", 
-	c.circ_modifier AS "Circulation Modifier", 
 	c.barcode AS "Barcode", 
-	xpath('//m:datafield[@tag="245"]/m:subfield[@code="a"]/text()', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]) as "Title", 'Missing circ mod' as "Issue"
+	xpath('//m:datafield[@tag="245"]/m:subfield[@code="a"]/text()', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]) as "Title", 
+	'Not Assigned' as "Circulation Modifier"
   FROM biblio.record_entry b
 	INNER JOIN asset.call_number cn ON cn.record=b.id
 	INNER JOIN asset.copy c ON c.call_number=cn.id
