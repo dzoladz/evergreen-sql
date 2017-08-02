4eaabf904672a4960f175b93d63b56d3bc4e56bb
dzoladz <derek@derekzoladz.com>
2017-08-02 10:12:36 -0400

    filter out deleted records

--- odl-imports-tcn-by-date.sql
+++ odl-imports-tcn-by-date.sql
@@ -1,5 +1,7 @@
-/* 
-Identify TCNs that have 'Overdrive' in the 856 
+/*
+EVERGREEN 2.10 - Consortium of Ohio Libraries 
+Identify TCNs that have 'Overdrive' in the 856
+Ohio Digital Library records are the only Overdrive records in the database
 Note: Query can take upword of 5 minutes to complete for yearly ranges 
 */
 SELECT 
@@ -12,6 +14,7 @@ WHERE (bre.source = '1' OR bre.source is null)
 	-- AND (bre.create_date BETWEEN '2017-01-01' AND '2017-03-01')
 	AND mrfr.tag = '856'
 	AND mrfr.value like '%overdrive%'
+	AND deleted IS FALSE
 ORDER BY bre.create_date DESC
 -- Limit 5
 ;
