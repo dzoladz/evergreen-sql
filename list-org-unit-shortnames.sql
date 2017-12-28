e61a6d803fdcb943c31800f79b11949d13a58155
dzoladz <derek@derekzoladz.com>
2017-12-28 11:13:15 -0500

    List the Shortname for each Library

---
+++ list-org-unit-shortnames.sql
@@ -0,0 +1,12 @@
+﻿/* Consortium of Ohio Libraries
+ * Version: Evergreen 3.0
+ * 
+ * Description: 
+ * List Shortname of Organizational Units
+ */
+SELECT 
+	shortname AS "Shortname ID", 
+	name AS "Library Name"
+FROM actor.org_unit
+WHERE ou_type = '3' -- main branch
+;
