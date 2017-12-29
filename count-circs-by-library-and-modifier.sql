ac1633f80541d26dd9abbce1130f7287604e9e38
dzoladz <derek@derekzoladz.com>
2017-12-29 15:44:53 -0500

    Updated for Evergreen 3.0

--- count-circs-by-library-and-modifier.sql
+++ count-circs-by-library-and-modifier.sql
@@ -1,27 +1,31 @@
--- EG version: 2.10
--- Count Total Circs for DVDs, display title and subtitle
+/* Consortium of Ohio Libraries
+ * Version: Evergreen 3.0
+ * 
+ * Description: 
+ * Count Total Circs for DVDs, display title and subtitle
+ */
 
-select 
+SELECT 
 -- *
-count(distinct(acirc.id)) AS "Total Circs",
+COUNT(DISTINCT(acirc.id)) AS "Total Circs",
 acn.label AS "Call Number",
 ac.barcode AS "Barcode",
-to_char(max(acirc.checkin_time), 'MM-DD-YYYY') AS "Last Checkin Time",
-max(acirc.due_date) > now() AS "Checked Out?",
+to_char(MAX(acirc.checkin_time), 'MM-DD-YYYY') AS "Last Checkin Time",
+MAX(acirc.due_date) > now() AS "Checked Out?",
   ARRAY_TO_STRING(
     XPATH('//marc:datafield[@tag="245"][1]/marc:subfield[@code="a" or @code="b"]/text()',
       bre.marc::XML, ARRAY[ARRAY['marc', 'http://www.loc.gov/MARC21/slim']]
     ),
     ' '
   ) AS "Detailed Info"
-from asset.copy ac
-inner join asset.call_number acn ON ac.call_number = acn.id
-inner join biblio.record_entry bre ON acn.record = bre.id
-inner join action.circulation acirc ON ac.id = acirc.target_copy
+FROM asset.copy ac
+INNER JOIN asset.call_number acn ON ac.call_number = acn.id
+INNER JOIN biblio.record_entry bre ON acn.record = bre.id
+INNER JOIN action.circulation acirc ON ac.id = acirc.target_copy
 where acn.owning_lib = '118'
 	AND ac.circ_modifier = 'Movie'
 	AND ac.deleted is false
-group by acirc.target_copy, acn.label, ac.barcode, bre.marc
-Order by "Total Circs" desc
+GROUP BY acirc.target_copy, acn.label, ac.barcode, bre.marc
+ORDER BY "Total Circs" DESC
 -- limit 5
 ;
