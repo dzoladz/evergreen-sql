2e79aa14d1c638302fb367f86984cbd62af6e02a
dzoladz <derek@derekzoladz.com>
2018-01-30 16:33:18 -0500

    Adjusted formatting

--- items-missing-circmods.sql
+++ items-missing-circmods.sql
@@ -13,12 +13,12 @@ SELECT aou.name AS "Owning Library",
 	ARRAY_TO_STRING(
 	XPATH('//m:datafield[@tag="245"]/m:subfield[@code="a"]/text()', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]), ' ') as "Title", 
 	'Not Assigned' as "Circulation Modifier"
-  FROM biblio.record_entry b
+FROM biblio.record_entry b
 	INNER JOIN asset.call_number cn ON cn.record=b.id
 	INNER JOIN asset.copy c ON c.call_number=cn.id
 	INNER JOIN actor.org_unit aou on aou.id=cn.owning_lib
-  WHERE b.deleted=FALSE
+WHERE b.deleted=FALSE
 	AND c.deleted=FALSE
 	AND b.id != -1
 	AND c.circ_modifier IS NULL
-  ORDER BY aou.name, c.barcode;
+ORDER BY aou.name, c.barcode;
