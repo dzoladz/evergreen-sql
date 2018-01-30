c815854c940c500384105035eda6302e6cb67dce
dzoladz <derek@derekzoladz.com>
2018-01-30 16:11:11 -0500

    Fixed the display of Titles in the output

--- items-with-undefined-copy-location.sql
+++ items-with-undefined-copy-location.sql
@@ -7,18 +7,19 @@
  * Note: This is an undefined copy location in COOL, no one should be using it
  */
  
-SELECT ou.name AS "Library", 
-	b.id AS "TCN", 
-	cn.label AS "Call Number", 
-	c.barcode AS "Barcode", 
-	xpath('//m:datafield[@tag="245"]/m:subfield[@code="a"]/text()', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]) AS "Title", 
-	'Copy Location ''Stacks'' is undefined' AS "Issue"
-  FROM biblio.record_entry b
-	INNER JOIN asset.call_number cn ON cn.record=b.id
-	INNER JOIN asset.copy c ON c.call_number=cn.id
-	INNER JOIN actor.org_unit ou on ou.id=c.circ_lib
-  WHERE b.deleted=FALSE
-	AND c.deleted=FALSE
-	AND b.id != -1
-	AND c.location = 1 -- i.e. Stacks
-  ORDER BY ou.name, c.barcode;
+SELECT ou.name AS "Library",
+	b.id AS "TCN",
+    cn.label AS "Call Number",
+    c.barcode AS "Barcode",
+    ARRAY_TO_STRING(
+    XPATH('//m:datafield[@tag="245"]/m:subfield[@code="a"]/text()', b.marc::xml, ARRAY[ARRAY['m','http://www.loc.gov/MARC21/slim']]), ' ') AS "Title",
+    'Copy Location ''Stacks'' is undefined' AS "Issue"
+FROM biblio.record_entry b
+    INNER JOIN asset.call_number cn ON cn.record=b.id
+    INNER JOIN asset.copy c ON c.call_number=cn.id
+    INNER JOIN actor.org_unit ou on ou.id=c.circ_lib
+WHERE b.deleted=FALSE
+    AND c.deleted=FALSE
+    AND b.id != -1
+    AND c.location = 1 -- i.e. Stacks
+ORDER BY ou.name, c.barcode;
