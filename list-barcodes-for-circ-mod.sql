3731548f9f80625e3a2329947f80295e39f84156
dzoladz <derek@derekzoladz.com>
2018-02-07 16:24:20 -0500

    Updated Circulation Modifier

--- list-barcodes-for-circ-mod.sql
+++ list-barcodes-for-circ-mod.sql
@@ -1,5 +1,11 @@
--- List the barcodes for items having a specific circulation modifer by library and shelving location
--- Useful for preparing circulation policy changes
+/* Consortium of Ohio Libraries
+ * Version: Evergreen 3.0
+ *
+ * Description:
+ * List the barcodes for items having a specific circulation modifer by library and shelving location
+ * Useful for preparing circulation policy changes
+ */
+
 SELECT 
 	aou.name AS "Library",
 	ac.barcode AS "Barcode",
@@ -10,12 +16,11 @@ SELECT
 	acl.name AS "Shelving Location",
 	acl.opac_visible AS "Shelving Location - OPAC Visible?",
 	acl.holdable AS "Shelving Location - Holdable?"
--- *
 FROM asset.copy ac
 	JOIN asset.copy_location acl ON ac.location = acl.id
 	JOIN actor.org_unit aou ON ac.circ_lib = aou.id
-WHERE ac.circ_modifier ~* 'equipment' -- set circ mod
-	AND aou.name ~* 'sunbury' -- set library
+WHERE ac.circ_modifier ~* 'movie-r' -- SET CIRCULATION MODIFIER
+	AND aou.name ~* 'j.r.' -- SET LIBRARY
 	AND ac.deleted = 'f'
 ORDER BY ac.edit_date DESC -- sort most recently added items to top
 -- limit 10
