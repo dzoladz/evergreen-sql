a7c7ec7d60ea9ea6b57a7856b85f73a9ad038a11
dzoladz <derek@derekzoladz.com>
2018-03-20 13:10:54 -0400

    add filters for library name/consortium

--- settings/library-settings-editor-values.sql
+++ settings/library-settings-editor-values.sql
@@ -14,6 +14,9 @@ SELECT
 	aorg.shortname AS "Library Shortname",
 	cous.grp AS "Associated Interface"
 FROM actor.org_unit_setting asetting
-INNER JOIN actor.org_unit aorg ON aorg.id = asetting.org_unit
-INNER JOIN config.org_unit_setting_type cous ON asetting.name = cous.name
-ORDER BY asetting.name,aorg.shortname ASC;
+	INNER JOIN actor.org_unit aorg ON aorg.id = asetting.org_unit
+	INNER JOIN config.org_unit_setting_type cous ON asetting.name = cous.name
+WHERE aorg.name ~* 'consortium' -- show all consortial policies
+	OR aorg.name ~* 'arcanum' -- useful to capture system and branch setting, if string in name is similar.
+ORDER BY aorg.name DESC, asetting.name ASC;
+
