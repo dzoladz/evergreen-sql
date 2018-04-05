5e5f41a0ddcb470bd61d97192f011fea1d3e7a41
dzoladz <derek@derekzoladz.com>
2018-04-05 13:57:47 -0400

    switch to right join

--- users/count-patron-by-permissions-group.sql
+++ users/count-patron-by-permissions-group.sql
@@ -10,7 +10,7 @@ SELECT pgt.id AS "Profile ID",
 	pgt.name AS "Permission Group",
 	count(au.profile) AS "# of Records"
 FROM actor.usr au
-	INNER JOIN permission.grp_tree pgt ON pgt.id = au.profile
+	RIGHT JOIN permission.grp_tree pgt ON pgt.id = au.profile
 --WHERE au.home_ou = '118' -- Or, specify a library J.R. Clarke
 GROUP BY pgt.name, pgt.id
 ORDER BY pgt.id;
