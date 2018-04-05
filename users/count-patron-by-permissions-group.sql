3fe40e9746e2c7dd526e8ddf1c3457e02bdb2253
dzoladz <derek@derekzoladz.com>
2018-04-05 13:38:20 -0400

    Default to searching the consortium

--- users/count-patron-by-permissions-group.sql
+++ users/count-patron-by-permissions-group.sql
@@ -2,13 +2,15 @@
  * Version: Evergreen 3.0
  * 
  * Description: 
- * Count User Records Assigned to a Permissions Group by Home Library
+ * List Permissions Groups, IDs, and Associated Record Counts
+ * Or, Filter by Home Library
  */
- 
-SELECT  
+
+SELECT pgt.id AS "Profile ID",
 	pgt.name AS "Permission Group",
 	count(au.profile) AS "# of Records"
 FROM actor.usr au
 	INNER JOIN permission.grp_tree pgt ON pgt.id = au.profile
-WHERE au.home_ou = '118' -- J.R. Clarke
-GROUP BY pgt.name;
+--WHERE au.home_ou = '118' -- Or, specify a library J.R. Clarke
+GROUP BY pgt.name, pgt.id
+ORDER pgt.id;
