8b102f6ce3ea657038d55d052624342dde09efd3
dzoladz <derek@derekzoladz.com>
2018-05-14 19:05:24 -0400

    add check to ensure hold not cancelled

--- holds/identify-stale-holds.sql
+++ holds/identify-stale-holds.sql
@@ -4,4 +4,5 @@ select *
 from action.hold_request
 where request_time > (now() - INTERVAL '200 days')
 and capture_time is NULL
-and expire_time is NULL;
+and expire_time is NULL
+and cancel_time is NULL;
