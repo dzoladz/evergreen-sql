cb7dbc20c060da75b369a4b15a014687ae62b5fc
dzoladz <derek@derekzoladz.com>
2018-05-14 19:14:46 -0400

    join asset.copy table to query

--- holds/identify-stale-holds.sql
+++ holds/identify-stale-holds.sql
@@ -3,7 +3,9 @@
 select *
 from action.hold_request ahold
 inner join actor.usr ausr on ausr.id = ahold.usr
+left join asset.copy acopy on acopy.id = ahold.target
 where ahold.request_time > (now() - INTERVAL '200 days')
 and ahold.capture_time is NULL
 and ahold.expire_time is NULL
 and ahold.cancel_time is NULL;
+
