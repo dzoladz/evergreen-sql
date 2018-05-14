8aac01d30e8bfbda8691cc37192ece1f43db810b
dzoladz <derek@derekzoladz.com>
2018-05-14 19:10:16 -0400

    join actor.usr table

--- holds/identify-stale-holds.sql
+++ holds/identify-stale-holds.sql
@@ -1,8 +1,9 @@
 -- find stale holds
 
 select *
-from action.hold_request
-where request_time > (now() - INTERVAL '200 days')
-and capture_time is NULL
-and expire_time is NULL
-and cancel_time is NULL;
+from action.hold_request ahold
+inner join actor.usr ausr on ausr.id = ahold.usr
+where ahold.request_time > (now() - INTERVAL '200 days')
+and ahold.capture_time is NULL
+and ahold.expire_time is NULL
+and ahold.cancel_time is NULL;
