689d9f4d1b5d2311492e6c86a429b3e4d7a852d0
dzoladz <derek@derekzoladz.com>
2018-05-14 19:20:24 -0400

    updated query descriptive notes

--- holds/identify-stale-holds.sql
+++ holds/identify-stale-holds.sql
@@ -1,11 +1,12 @@
 -- find stale holds
+-- Add check for 'frozen' holds
 
-select *
-from action.hold_request ahold
-inner join actor.usr ausr on ausr.id = ahold.usr
-left join asset.copy acopy on acopy.id = ahold.target
-where ahold.request_time > (now() - INTERVAL '200 days')
-and ahold.capture_time is NULL
-and ahold.expire_time is NULL
-and ahold.cancel_time is NULL;
+SELECT *
+FROM action.hold_request ahold
+INNER JOIN actor.usr ausr ON ausr.id = ahold.usr
+LEFT JOIN asset.copy acopy ON acopy.id = ahold.target
+WHERE ahold.request_time > (now() - INTERVAL '200 days')
+AND ahold.capture_time is NULL
+AND ahold.expire_time is NULL
+AND ahold.cancel_time is NULL;
 
