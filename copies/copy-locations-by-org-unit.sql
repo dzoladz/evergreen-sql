e8b5b019b9661dcad8ac727125fe0806e8173c03
dzoladz <derek@derekzoladz.com>
2018-08-21 10:00:47 -0400

    Add location id to output

--- copies/copy-locations-by-org-unit.sql
+++ copies/copy-locations-by-org-unit.sql
@@ -1,8 +1,12 @@
 -- Copy locations by Organizational Unit
 SELECT 
+  acl.id AS "Location ID",
   acl.name AS "Shelving/Copy Location", 
   aou.name AS "Library"
 FROM asset.copy_location acl
   INNER JOIN actor.org_unit aou ON acl.owning_lib = aou.id
 WHERE acl.deleted = 'f'
-ORDER BY aou.name ASC;
+	AND aou.name ~* 'cardington' -- Library
+--ORDER BY aou.name ASC
+ORDER BY acl.name ASC
+;
