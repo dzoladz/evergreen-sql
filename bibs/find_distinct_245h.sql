5658061f7fb874d6b5f7acf5f6c8d62d6af997a2
dzoladz <derek@derekzoladz.com>
2018-09-04 10:48:17 -0400

    remove deleted records from output and select TCNs into an array

--- bibs/find_distinct_245h.sql
+++ bibs/find_distinct_245h.sql
@@ -1,13 +1,19 @@
 /* Derek C. Zoladz
  * Evergreen 3.0.x
  *
- * List distinct 245 $h values
- * NOTE: Includes deleted records to give a broader perspective of use
+ * List distinct 245 $h values and list TCNs
+ * NOTE: Removed values for any deleted records
  */
-SELECT distinct(value), count(record)
-FROM metabib.real_full_rec
-WHERE tag = '245'
+SELECT
+	distinct(mrfr.value) AS "245 $h",
+	count(mrfr.record),
+	array_agg(mrfr.record) AS "TCNs"
+FROM metabib.real_full_rec mrfr
+JOIN biblio.record_entry bre ON mrfr.record=bre.tcn_value::integer
+WHERE bre.deleted = FALSE
+	AND mrfr.tag = '245'
 	AND subfield = 'h'
 GROUP BY value
-ORDER BY value ASC
+HAVING count(mrfr.record) < 16
+ORDER BY count ASC
 ;
