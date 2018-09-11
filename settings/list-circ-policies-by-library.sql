283076bfe1c7c59e04a815f092bf88bf665dcc0c
dzoladz <derek@derekzoladz.com>
2018-09-11 14:29:55 -0400

    filter out any rules that are inactive from the output

--- settings/list-circ-policies-by-library.sql
+++ settings/list-circ-policies-by-library.sql
@@ -1,5 +1,5 @@
 /* Consortium of Ohio Libraries
- * Version: Evergreen 3.0
+ * Version: Evergreen 3.1
  *
  * Description:
  * Lists Circulation Policies by Library
@@ -29,5 +29,8 @@ FROM config.circ_matrix_matchpoint ccmm
 	INNER JOIN config.rule_circ_duration crcd ON crcd.id = ccmm.duration_rule
 	INNER JOIN config.rule_max_fine crmf ON crmf.id = ccmm.max_fine_rule
 	INNER JOIN config.rule_recurring_fine crrf ON crrf.id = ccmm.recurring_fine_rule
-WHERE org_unit = '118' -- J.R. Clarke, id is preferred here
+WHERE org_unit = '101' -- Germantown, id is preferred here
+	AND ccmm.active IS TRUE -- filter out disabled rules
+	-- AND ccmm.user_home_ou = '1' -- rules applicable to the consortium
+ORDER BY pgt.name, ccmm.circ_modifier
 ;
